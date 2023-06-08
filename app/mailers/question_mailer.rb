ec29b5a31423835d475499164baeeba2e82c5119
linlin <1208215066@qq.com>
2023-06-08 11:49:45 +0800

    修改了登录退出，尚未完善

--- app/mailers/question_mailer.rb
+++ app/mailers/question_mailer.rb
@@ -3,6 +3,6 @@ class QuestionMailer < ApplicationMailer
   def question_assigned(question)
     @question = question
     @project = question.project
-    mail(to: @question.assigned_to, subject: "您有一项新的任务 - #{@question}")
+    mail(to: @question.assigned_to, subject: "您有一项新的任务 - #{@question.title}")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+ # devise_for :users
   resources :questions
   resources :projects
   resources :myfiles
   root "projects#index"
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
